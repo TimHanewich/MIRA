85f0713150ef0473affb3d8e84758ae44e63e1b9
Tim Hanewich <tahanewich@live.com>
2026-05-01 21:26:14 -0400

    Fixed out

--- src/Program.cs
+++ src/Program.cs
@@ -121,7 +121,7 @@ namespace AIA
                 //Print consumption metrics
                 AnsiConsole.MarkupLine("[underline][gray]Cumulative Consumption[/][/]");
                 AnsiConsole.MarkupLine("[gray]Input Tokens: " + state.InputTokensConsumed.ToString("#,##0") + "[/]");
-                AnsiConsole.MarkupLine("[gray]Output Tokens: " + state.InputTokensConsumed.ToString("#,##0") + "[/]");
+                AnsiConsole.MarkupLine("[gray]Output Tokens: " + state.OutputTokensConsumed.ToString("#,##0") + "[/]");
             }
             else
             {
