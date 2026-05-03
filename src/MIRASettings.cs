79f4ce70f0b6e477ddae970a160b780ddbb54863
Tim Hanewich <tahanewich@live.com>
2026-05-03 12:34:39 -0400

    Default check

--- src/MIRASettings.cs
+++ src/MIRASettings.cs
@@ -5,9 +5,24 @@ namespace MIRA
 {
     public class MIRASettings
     {
-        public string? FoundryEndpoint {get; set;}
-        public string? FoundryApiKey {get; set;}
-        public string? FoundryModel {get; set;}
+        public string FoundryEndpoint {get; set;}
+        public string FoundryApiKey {get; set;}
+        public string FoundryModel {get; set;}
+
+        public MIRASettings()
+        {
+            FoundryEndpoint = "<foundry endpoint here, such as 'https://myservice.services.ai.azure.com'>";
+            FoundryApiKey = "<foundry API key here, such as 'CE6l5HWOMps0k8fhsFbCdWozJlhrjRdE6NCEqxvhGPRXSJQQJ99CDACHYHv6XJ3w3ADBDOG9VJq'>";
+            FoundryModel = "<foundry model deployment name here, such as 'gpt-5-mini'>";
+        }
+
+        //Determines if the "default" fill in values have not yet been added
+        //If only ONE of the fields is default, it will return true
+        public bool IsDefault()
+        {
+            MIRASettings ToCompareTo = new MIRASettings();
+            return FoundryEndpoint == ToCompareTo.FoundryEndpoint || FoundryApiKey == ToCompareTo.FoundryEndpoint || FoundryModel == ToCompareTo.FoundryEndpoint;
+        }
 
         public static string SavePath
         {
